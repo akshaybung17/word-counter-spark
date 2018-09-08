010a7412ab373add868692056f599b7a92ea8aeb
Akshay Bung <akshay.bung17@gmail.com>
2018-09-07 22:07:48 -0600

    added normalize words function

--- word-count.py
+++ word-count.py
@@ -1,10 +1,15 @@
 from pyspark import SparkConf, SparkContext
+import re
 
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
 
+def normalizeWords(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+
 input = sc.textFile("book.txt")
-words = input.flatMap(lambda x: x.split())
+words = input.flatMap(normalizeWords)
 wordCounts = words.countByValue()
 
 
