b147bc3deb9e66f3a509abb35ede847991da49c7
Akshay Bung <akshay.bung17@gmail.com>
2018-09-10 22:44:07 -0600

    added sorting method in order to get a more meaningful result

--- word-count.py
+++ word-count.py
@@ -1,19 +1,21 @@
-from pyspark import SparkConf, SparkContext
 import re
-
-conf = SparkConf().setMaster("local").setAppName("WordCount")
-sc = SparkContext(conf = conf)
+from pyspark import SparkConf, SparkContext
 
 def normalizeWords(text):
     return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
 
 input = sc.textFile("book.txt")
 words = input.flatMap(normalizeWords)
-wordCounts = words.countByValue()
 
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
+results = wordCountsSorted.collect()
 
-for word, count in wordCounts.items():
-    cleanWord = word.encode('ascii', 'ignore')
-    if (cleanWord):
-        print(cleanWord.decode() + " " + str(count))
+for result in results:
+    count = str(result[0])
+    word = result[1].encode('ascii', 'ignore')
+    if (word):
+        print(word.decode() + ":\t\t" + count)
